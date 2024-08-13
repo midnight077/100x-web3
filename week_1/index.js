cacfd7075096754f146d83471d3a0d6c5c03dbef
midnight077 <nishitj077@gmail.com>
2024-08-14 00:18:42 +0530

    week 1 - getting started with web3

---
+++ week_1/index.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+const input = "100xdevs";
+const hash = crypto.createHash('sha256').update(input).digest('hex');
+
+console.log(typeof hash);
+
+// whats the difference between bitcoin node and bitcoin miner
