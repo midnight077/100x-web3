2735c96c2fc31b93d0f46cb53c363dc7775ccf08
midnight077 <nishitj077@gmail.com>
2024-08-24 18:28:28 +0530

    encryption

---
+++ week_2/encryption.js
@@ -0,0 +1,17 @@
+// Hashing -> converting data into a fixed size O/P
+
+// encryption -> process of converting plaintext data into an unreadable format, called ciphertext, using a specific algorithm and a key. The data can be decrypted back to its original form only with the appropriate key.
+
+// Symmetric encryption -> The same key is used for both encryption and decryption.
+// Asymmetric encryption -> Different keys are used for encryption (private key) and decryption (public key).
+
+// private key is used to sign a transaction and public key can be used to verify/decode the transaction, so that the miner can verify that the transaction is legit
+
+// Common Asymmetric Encryption Algorithms:
+// RSA - Rivest–Shamir–Adleman
+// ECC - Elliptic Curve Cryptography (ECDSA) - ETH and BTC
+// EdDSA - Edwards-curve Digital Signature Algorithm  - SOL
+
+// Common eleptic curves
+// secp256k1 - BTC and ETH
+// ed25519 - SOL
